6944fadf25a9ed878ba3d7ec5e81c6cf56e950f2
AnasGuetarni <anas.guetarni@gmail.com>
2018-09-04 15:59:18 +0200

    Multi-users

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var session = require('express-session');
 var helmet = require('helmet');
 var bodyParser = require('body-parser');
 var colors = require('colors');
+var path = require('path');
 
 let Parser = require('rss-parser');
 let parser = new Parser();
@@ -19,7 +20,7 @@ var dateNow = new Date(Date.now());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname + "/public")));
 app.set('view engine', 'ejs');
 
 app.use(session({
@@ -34,6 +35,9 @@ console.log("\nLoading custom modules:".blue);
 var db = require('./lib/db');
 var types = require('./lib/types/');
 var router = require('./lib/router')(app);
+var dataAccess = require('./lib/dataAccess');
+var socket = require('./lib/socket')(http);
+var functions = require('./lib/functions');
 
 http.listen(global.PORT,function(){
     console.log(('\n' + "=== App Started on PORT " + global.PORT + " at " + dateNow + " ===" + '\n').green);
